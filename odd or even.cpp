58b3f9a6684054d88fc51e87ce1b102a597330ee
BALAJI1314 <123939853+BALAJI1314@users.noreply.github.com>
2023-08-21 10:38:48 +0530

    Add files via upload

---
+++ odd or even.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int numbers[n];
+
+    // Input numbers
+    printf("Enter %d numbers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    printf("Odd/Even results:\n");
+    for (int i = 0; i < n; i++) {
+        if (numbers[i] % 2 == 0) {
+            printf("%d is even.\n", numbers[i]);
+        } else {
+            printf("%d is odd.\n", numbers[i]);
+        }
+    }
+
+    return 0;
+}
+
